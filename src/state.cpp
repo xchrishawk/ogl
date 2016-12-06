b171bc09769f827f4d4c23282caa989df8d1892e
Chris Vig <chris@invictus.so>
2016-12-05 20:06:42 -0600

    Make every cube face a different color.

--- src/state.cpp
+++ src/state.cpp
@@ -37,30 +37,84 @@ state::state()
   : m_camera_rot(DEFAULT_CAMERA_ROT),
     m_camera_pos(DEFAULT_CAMERA_POS)
 {
-  static const vertex RGB_VERTICES[] =
+  static const float FRONT 	= 0.5f;
+  static const float BACK 	= -0.5f;
+  static const float RIGHT 	= 0.5f;
+  static const float LEFT 	= -0.5f;
+  static const float TOP	= 0.5f;
+  static const float BOTTOM	= -0.5f;
+
+  static const vertex FRONT_VERTICES[] =
   {
     //  X	Y	Z	R	G	B	A
-    {  	-0.5f,	0.5f, 	0.5f,	1.0f,	0.0f,	0.0f,	1.0f,	},	// 0 - front top left
-    { 	0.5f,	0.5f,	0.5f,	0.0f,	1.0f,	0.0f,	1.0f,	},	// 1 - front top right
-    {	-0.5f,	-0.5f,	0.5f,	0.0f,	0.0f,	1.0f,	1.0f,	},	// 2 - front bottom left
-    {	0.5f,	-0.5f,	0.5f,	1.0f,	1.0f,	1.0f,	1.0f,	},	// 3 - front bottom right
-    {  	-0.5f,	0.5f, 	-0.5f,	1.0f,	1.0f,	1.0f,	1.0f,	},	// 4 - back top left
-    { 	0.5f,	0.5f,	-0.5f,	1.0f,	1.0f,	0.0f,	1.0f,	},	// 5 - back top right
-    {	-0.5f,	-0.5f,	-0.5f,	0.0f,	1.0f,	1.0f,	1.0f,	},	// 6 - back bottom left
-    {	0.5f,	-0.5f,	-0.5f,	1.0f,	0.0f,	1.0f,	1.0f,	},	// 7 - back bottom right
+    {	LEFT,	TOP,	FRONT,	1.0f,	0.0f,	0.0f,	1.0f	},
+    {	RIGHT,	TOP,	FRONT,	1.0f,	0.0f,	0.0f,	1.0f	},
+    {	LEFT,	BOTTOM,	FRONT,	1.0f,   0.0f,	0.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	FRONT,	1.0f,	0.0f,	0.0f,	1.0f	},
+  };
+  static const GLuint FRONT_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(FRONT_VERTICES, ogl_array_size(FRONT_VERTICES),
+				  FRONT_INDICES, ogl_array_size(FRONT_INDICES)));
 
-  };
-  static const GLuint RGB_INDICES[] =
+  static const vertex BACK_VERTICES[] =
   {
-    0, 1, 2, 1, 2, 3,		// front face
-    4, 5, 6, 5, 6, 7,		// back face
-    0, 1, 4, 4, 5, 1,		// top face
-    2, 3, 6, 6, 7, 3,		// bottom face
-    0, 2, 4, 4, 6, 2,		// left face
-    1, 3, 5, 5, 7, 3,		// right face
+    //  X	Y	Z	R	G	B	A
+    {	LEFT,	TOP,	BACK,	1.0f,	1.0f,	0.0f,	1.0f	},
+    {	RIGHT,	TOP,	BACK,	1.0f,	1.0f,	0.0f,	1.0f	},
+    {	LEFT,	BOTTOM,	BACK,	1.0f,   1.0f,	0.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	BACK,	1.0f,	1.0f,	0.0f,	1.0f	},
   };
-  m_meshes.push_back(mesh::create(RGB_VERTICES, ogl_array_size(RGB_VERTICES),
-				  RGB_INDICES, ogl_array_size(RGB_INDICES)));
+  static const GLuint BACK_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(BACK_VERTICES, ogl_array_size(BACK_VERTICES),
+				  BACK_INDICES, ogl_array_size(BACK_INDICES)));
+
+  static const vertex LEFT_VERTICES[] =
+  {
+    //  X	Y	Z	R	G	B	A
+    {	LEFT,	TOP,	FRONT,	0.0f,	1.0f,	0.0f,	1.0f	},
+    {	LEFT,	BOTTOM,	FRONT,	0.0f,	1.0f,	0.0f,	1.0f	},
+    {	LEFT,	TOP,	BACK,	0.0f,   1.0f,	0.0f,	1.0f	},
+    {	LEFT,	BOTTOM,	BACK,	0.0f,	1.0f,	0.0f,	1.0f	},
+  };
+  static const GLuint LEFT_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(LEFT_VERTICES, ogl_array_size(LEFT_VERTICES),
+				  LEFT_INDICES, ogl_array_size(LEFT_INDICES)));
+
+  static const vertex RIGHT_VERTICES[] =
+  {
+    //  X	Y	Z	R	G	B	A
+    {	RIGHT,	TOP,	FRONT,	0.0f,	1.0f,	1.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	FRONT,	0.0f,	1.0f,	1.0f,	1.0f	},
+    {	RIGHT,	TOP,	BACK,	0.0f,   1.0f,	1.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	BACK,	0.0f,	1.0f,	1.0f,	1.0f	},
+  };
+  static const GLuint RIGHT_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(RIGHT_VERTICES, ogl_array_size(RIGHT_VERTICES),
+				  RIGHT_INDICES, ogl_array_size(RIGHT_INDICES)));
+
+  static const vertex TOP_VERTICES[] =
+  {
+    //  X	Y	Z	R	G	B	A
+    {	LEFT,	TOP,	FRONT,	0.0f,	0.0f,	1.0f,	1.0f	},
+    {	RIGHT,	TOP,	FRONT,	0.0f,	0.0f,	1.0f,	1.0f	},
+    {	LEFT,	TOP,	BACK,	0.0f,   0.0f,	1.0f,	1.0f	},
+    {	RIGHT,	TOP,	BACK,	0.0f,	0.0f,	1.0f,	1.0f	},
+  };
+  static const GLuint TOP_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(TOP_VERTICES, ogl_array_size(TOP_VERTICES),
+				  TOP_INDICES, ogl_array_size(TOP_INDICES)));
+
+  static const vertex BOTTOM_VERTICES[] =
+  {
+    //  X	Y	Z	R	G	B	A
+    {	LEFT,	BOTTOM,	FRONT,	1.0f,	0.0f,	1.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	FRONT,	1.0f,	0.0f,	1.0f,	1.0f	},
+    {	LEFT,	BOTTOM,	BACK,	1.0f,   0.0f,	1.0f,	1.0f	},
+    {	RIGHT,	BOTTOM,	BACK,	1.0f,	0.0f,	1.0f,	1.0f	},
+  };
+  static const GLuint BOTTOM_INDICES[] = { 0, 1, 2, 1, 2, 3 };
+  m_meshes.push_back(mesh::create(BOTTOM_VERTICES, ogl_array_size(BOTTOM_VERTICES),
+				  BOTTOM_INDICES, ogl_array_size(BOTTOM_INDICES)));
 }
 
 void state::loop(float abs_t, float delta_t, const key_input& key_input)
