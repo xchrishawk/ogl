ad8674dfd7754129ff9f0e1ea209e61d5b22ba65
Chris Vig <chris@invictus.so>
2016-12-16 14:33:56 -0600

    Cleanup.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,48 +3,9 @@
  * Chris Vig (chris@invictus.so)
  */
 
-/* -- Includes -- */
-
-#include <cstdlib>
-#include <exception>
-#include <sstream>
-#include <string>
-
-#include "app/application.hpp"
-#include "opengl/opengl.hpp"
-#include "util/debug.hpp"
-
-/* -- Namespaces -- */
-
-using namespace ogl;
-
 /* -- Procedures -- */
 
 int main(int argc, char** argv)
 {
-  try
-  {
-    // required initialization objects
-    glfw_factory glfw_fact;
-    opengl_factory& ogl_fact = opengl_factory::platform_default();
-
-    // create and run application
-    application app(ogl_factory);
-    app.main();
-
-    // successful app run!
-    return EXIT_SUCCESS;
-  }
-  catch (const std::exception& ex)
-  {
-    // print debug info if needed
-#if defined(OGL_DEBUG)
-    std::ostringstream message;
-    message << "Uncaught exception: " << ex.what();
-    ogl_fail_message(message.str());
-#endif /* defined(OGL_DEBUG) */
-
-    // failed...
-    return EXIT_FAILURE;
-  }
+  return 0;
 }
