3818595be5a189833d4a06c3061306c84dc72b9c
Chris Vig <chris@invictus.so>
2016-12-15 19:55:12 -0600

    Implement random module.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,6 +13,25 @@
 int main(int argc, char** argv)
 {
   ogl::random_init();
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_int() << std::endl;
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_int(15) << std::endl;
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_float() << std::endl;
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_float(10.0f) << std::endl;
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_double() << std::endl;
+
+  for (int idx = 0; idx < 10; idx++)
+    std::cout << ogl::random_double(200.0) << std::endl;
+
   std::cout << "hello ogl" << std::endl;
   return 0;
 }
--- src/util/random.hpp
+++ src/util/random.hpp
@@ -10,6 +10,25 @@
 namespace ogl
 {
 
+  /** Initializes the random number generator. */
   void random_init();
+
+  /** Returns a random number between 0 and RAND_MAX. */
+  int random_int();
+
+  /** Returns a random number between 0 and max. */
+  int random_int(int max);
+
+  /** Returns a random float between 0 and 1. */
+  float random_float();
+
+  /** Returns a random float between 0 and max. */
+  float random_float(float max);
+
+  /** Returns a random double between 0 and 1. */
+  double random_double();
+
+  /** Returns a random double between 0 and max. */
+  double random_double(double max);
 
 }
