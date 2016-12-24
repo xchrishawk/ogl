f78c2694f7eeacd4f35d6536d9e45d30a718f8d1
Chris Vig <chris@invictus.so>
2016-12-23 18:57:02 -0600

    Add stub for application library.

---
+++ src/app/application.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file	application.cpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2016/12/23
+ */
+
+/* -- Includes -- */
+
+#include "application.hpp"
---
+++ src/app/application.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file	application.hpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2016/12/23
+ */
+
+#pragma once
+
+/* -- Types -- */
+
+namespace ogl
+{
+
+  /**
+   * Main class for the OGL application.
+   */
+  class application
+  {
+
+  };
+
+}
