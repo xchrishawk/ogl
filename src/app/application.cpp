f8b431393c12c51e87e35560af843a0dcb223d2a
Chris Vig <chris@invictus.so>
2016-12-27 15:46:20 -0600

    Add app library.

---
+++ src/app/application.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file	application.cpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2016/12/27
+ */
+
+/* -- Includes -- */
+
+#include "app/application.hpp"
---
+++ src/app/application.hpp
@@ -0,0 +1,7 @@
+/**
+ * @file	application.hpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2016/12/27
+ */
+
+#pragma once
