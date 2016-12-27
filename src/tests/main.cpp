c97910dfcbe8fdde170d955ff2ce3b83a31215cb
Chris Vig <chris@invictus.so>
2016-12-27 14:20:00 -0600

    Initial directory structure.

---
+++ src/tests/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file	main.cpp
+ * @author	Chris Vig (chris@invictus.so)
+ * @date	2016/12/27
+ */
+
+/* -- Includes -- */
+
+#include <iostream>
+
+/* -- Procedures -- */
+
+int main(int argc, char** argv)
+{
+  std::cout << "tests" << std::endl;
+  return 0;
+}
