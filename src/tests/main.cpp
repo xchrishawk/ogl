69102b9d0f9697b395139dc83b2d5d63582f976e
Chris Vig <chris@invictus.so>
2016-12-27 14:48:07 -0600

    Configure Google Test for unit tests.

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -6,12 +6,26 @@
 
 /* -- Includes -- */
 
-#include <iostream>
+#include <gtest/gtest.h>
+
+/* -- Test Case -- */
+
+TEST(SampleTestCase, ShouldPass)
+{
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(2, 2);
+}
+
+TEST(SampleTestCase, ShouldFail)
+{
+  ASSERT_EQ(1, 2);
+  ASSERT_EQ(2, 1);
+}
 
 /* -- Procedures -- */
 
 int main(int argc, char** argv)
 {
-  std::cout << "tests" << std::endl;
-  return 0;
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
